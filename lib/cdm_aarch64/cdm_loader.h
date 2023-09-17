0cd116435f2abe0c414bc8cfd28229644abc66ca
CastagnaIT <gottardo.stefano.83@gmail.com>
2023-09-17 14:31:10 +0200

    removed class

--- lib/cdm_aarch64/cdm_loader.h
+++ lib/cdm_aarch64/cdm_loader.h
@@ -10,24 +10,24 @@
 
 #include <cstdint>
 
-class CCdmAarch64Loader
+extern "C"
 {
-  extern "C"
-  {
 // Linux arm64 version of libwidevinecdm.so depends on two dynamic symbols.
 // See https://github.com/xbmc/inputstream.adaptive/issues/1128
-#if defined(__linux__) && defined(__aarch64__) && !defined(ANDROID)
-    __attribute__((target("no-outline-atomics"))) int32_t __aarch64_ldadd4_acq_rel(int32_t value,
-                                                                                   int32_t* ptr)
-    {
-      return __atomic_fetch_add(ptr, value, __ATOMIC_ACQ_REL);
-    }
+#if defined(__linux__)
+#if defined(__aarch64__) || defined(__arm64__)
 
-    __attribute__((target("no-outline-atomics"))) int32_t __aarch64_swp4_acq_rel(int32_t value,
+  __attribute__((target("no-outline-atomics"))) int32_t __aarch64_ldadd4_acq_rel(int32_t value,
                                                                                  int32_t* ptr)
-    {
-      return __atomic_exchange_n(ptr, value, __ATOMIC_ACQ_REL);
-    }
-#endif
+  {
+    return __atomic_fetch_add(ptr, value, __ATOMIC_ACQ_REL);
   }
-};
+
+  __attribute__((target("no-outline-atomics"))) int32_t __aarch64_swp4_acq_rel(int32_t value,
+                                                                               int32_t* ptr)
+  {
+    return __atomic_exchange_n(ptr, value, __ATOMIC_ACQ_REL);
+  }
+#endif
+#endif
+}
